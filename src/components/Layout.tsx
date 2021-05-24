3db97d16b8ed2c09b4ab8eb80fbb13113dee0a6c
Vadim <nevod80@gmail.com>
2021-05-24 18:48:01 +0700

    setup simple app

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,22 +19,12 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         <Link href="/">
           <a>Home</a>
         </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+        <Link href="/dashboard">
+          <a>Dashboard</a>
+        </Link>
       </nav>
     </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
   </div>
 )
 
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Theme from '../Theme';
+
+export default function MyApp({ 
+  Component, 
+  pageProps,
+}: AppProps) {
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={Theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
